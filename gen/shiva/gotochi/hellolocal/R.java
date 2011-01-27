f808bef81a92e59a69ad15f740ec9736421009bd
Shinobu Izumi <stagesp1@gmail.com>
2011-01-28 00:04:45 +0900

    Merging library made by Unity.

--- gen/shiva/gotochi/hellolocal/R.java
+++ gen/shiva/gotochi/hellolocal/R.java
@@ -18,22 +18,16 @@ public final class R {
         public static final int app_splash=0x7f020001;
         public static final int icon=0x7f020002;
     }
-    public static final class id {
-        public static final int inputLayout=0x7f070000;
-        public static final int okButton=0x7f070002;
-        public static final int txtInput=0x7f070001;
-    }
     public static final class integer {
         public static final int gles_mode=0x7f050000;
         public static final int splash_mode=0x7f050001;
     }
     public static final class layout {
-        public static final int input=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int done_button=0x7f040001;
-        public static final int hello=0x7f040002;
+        public static final int app_name=0x7f040001;
+        public static final int done_button=0x7f040002;
+        public static final int hello=0x7f040000;
     }
 }
